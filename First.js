4d19a4c1f919e60184859d5f7329db9c5072b80d
chandana kurakula <chandana kurakula>
2017-04-20 20:02:07 -0700

    abc

---
+++ First.js
@@ -0,0 +1,7 @@
+var add = function(i,j){
+    return i+j;
+}
+
+var k = add(2,3);
+
+exports.adding = add;
---
+++ test/myTest.js
@@ -0,0 +1,12 @@
+
+var tt = require('../First')
+var assert = require('assert')
+
+describe('check addition', function(){
+    describe('check if addition result is correct', function(){
+    it('result should be 5', function(){
+        assert.equal(6, tt.adding(3,2));
+
+    });
+    });
+});
